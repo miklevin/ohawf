d2062797c79f5587031e3ce255bc0b06b8b1da3d
Mike Levin <miklevin@hotmail.com>
2021-09-25 16:41:57 -0400

    Release again

--- OhAwf/_nbdev.py
+++ OhAwf/_nbdev.py
@@ -2,9 +2,9 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {}
+index = {"Credentials": "00_core.ipynb"}
 
-modules = []
+modules = ["core.py"]
 
 doc_url = "https://miklevin.github.io/ohawf/"
 

5cba6dec0e607e7953ba232f2c72bd67ffe97460
Mike Levin <miklevin@hotmail.com>
2021-09-25 23:10:39 -0400

    Putting get method back in

--- OhAwf/_nbdev.py
+++ OhAwf/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"Credentials": "00_core.ipynb"}
+index = {"Credentials": "00_core.ipynb",
+         "get": "00_core.ipynb"}
 
 modules = ["core.py"]
 
